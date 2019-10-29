4fbc188a81e4960fc4343eec24acd4da0a371e1b
Changkon Han <chang_kon@hotmail.com>
2019-10-30 00:10:42 +0900

    Create rest controller for admin. Except update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,31 @@
+module woorinaru.spring.rest {
+    // woorinaru models
+    requires woorinaru.core;
+    requires woorinaru.repository.sql;
+    // JPA
+    requires java.sql;
+    requires java.persistence;
+
+    // mapstruct
+    requires org.mapstruct;
+
+    // spring
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.web;
+    requires spring.beans;
+    requires spring.tx;
+    requires spring.orm;
+    requires spring.security.config;
+
+    requires java.annotation;
+
+    // jackson
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires com.fasterxml.jackson.datatype.jdk8;
+    requires com.fasterxml.jackson.module.paramnames;
+}
---
+++ src/main/java/woorinaru/rest/service/AdminService.java
@@ -0,0 +1,6 @@
+package woorinaru.rest.service;
+
+import woorinaru.rest.dto.user.Admin;
+
+public interface AdminService extends GenericService<Admin> {
+}
