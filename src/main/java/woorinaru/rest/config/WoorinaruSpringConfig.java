727af03f16d250caae3edb549d03e7ba7146cf66
Changkon Han <chang_kon@hotmail.com>
2019-11-02 01:35:23 +0900

    Create student rest controller

--- src/main/java/woorinaru/rest/config/WoorinaruSpringConfig.java
+++ src/main/java/woorinaru/rest/config/WoorinaruSpringConfig.java
@@ -2,17 +2,22 @@ package woorinaru.rest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import woorinaru.core.service.AdminService;
-import woorinaru.core.service.AdminServiceImpl;
-import woorinaru.core.service.ResourceService;
-import woorinaru.core.service.ResourceServiceImpl;
+import woorinaru.core.service.*;
 import woorinaru.repository.sql.dao.impl.AdminDaoImpl;
 import woorinaru.repository.sql.dao.impl.ResourceDaoImpl;
+import woorinaru.repository.sql.dao.impl.StudentDaoImpl;
 
 import javax.persistence.EntityManager;
 
 @Configuration
 public class WoorinaruSpringConfig {
+
+    @Bean
+    public StudentService studentService(EntityManager em) {
+        StudentServiceImpl studentService = new StudentServiceImpl();
+        studentService.setDao(new StudentDaoImpl(em));
+        return studentService;
+    }
 
     @Bean
     public AdminService adminService(EntityManager em) {
