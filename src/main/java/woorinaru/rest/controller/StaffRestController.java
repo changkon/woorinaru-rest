a8be7f7677e69af09042ad4a18726b7bdf108979
Changkon Han <chang_kon@hotmail.com>
2019-11-02 13:09:02 +0900

    Create staff controller

---
+++ src/main/java/woorinaru/rest/controller/StaffRestController.java
@@ -0,0 +1,59 @@
+package woorinaru.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import woorinaru.rest.dto.user.Staff;
+import woorinaru.rest.service.StaffService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffRestController {
+
+    @Autowired
+    private StaffService staffService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Staff> get(@PathVariable int id) {
+        Staff staffDto = this.staffService.get(id);
+        return ResponseEntity.ok().body(staffDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Staff>> getAll() {
+        List<Staff> staffDtos = this.staffService.getAll();
+        return ResponseEntity.ok().body(staffDtos);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody Staff staff) {
+        int generatedId = this.staffService.create(staff);
+        String uri = String.format("/woorinaru/api/staff/%d", generatedId);
+        return ResponseEntity.created(URI.create(uri)).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        Staff staff = new Staff();
+        staff.setId(id);
+        this.staffService.delete(staff);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<String> modify(@RequestBody Staff staff) {
+        this.staffService.modify(staff);
+        return ResponseEntity.ok().build();
+    }
+
+    public StaffService getStaffService() {
+        return staffService;
+    }
+
+    public void setStaffService(StaffService staffService) {
+        this.staffService = staffService;
+    }
+}
