0c4b0f53a91a55de5f9a136e8c75e07870ae5fd2
Changkon Han <chang_kon@hotmail.com>
2019-12-06 04:23:48 +0900

    Generate access token for stage environment

--- src/main/java/com/woorinaru/rest/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/woorinaru/rest/filter/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package com.woorinaru.rest.filter;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-@Profile("prod")
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
--- src/main/java/com/woorinaru/rest/service/impl/StageAuthenticationServiceImpl.java
+++ src/main/java/com/woorinaru/rest/service/impl/StageAuthenticationServiceImpl.java
@@ -1,9 +1,15 @@
 package com.woorinaru.rest.service.impl;
 
 import com.woorinaru.core.model.user.User;
+import com.woorinaru.rest.mapper.user.UserMapper;
 import com.woorinaru.rest.security.token.identity.IdTokenUtil;
 import com.woorinaru.rest.security.token.jwt.JwtTokenUtil;
 import com.woorinaru.rest.service.AuthenticationService;
+import com.woorinaru.rest.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -11,20 +17,29 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-@Service
-@Profile({"default", "dev", "prod"})
-public class AuthenticationServiceImpl implements AuthenticationService {
+@Service("authenticationServiceImpl")
+@Profile({"stage"})
+public class StageAuthenticationServiceImpl implements AuthenticationService {
 
     private IdTokenUtil idTokenUtil;
     private JwtTokenUtil jwtTokenUtil;
+    private UserService userService;
 
-    public AuthenticationServiceImpl() {}
+    public StageAuthenticationServiceImpl() {}
 
     @Transactional
     @Override
     public String generateAccessToken(Optional<String> idToken) {
         if (idToken.isPresent()) {
-            User user = idTokenUtil.retrieveUserAndCreateIfNotExists(idToken.get());
+            // Get email field from JWT token
+            String jws = idToken.get();
+            int i = jws.lastIndexOf('.');
+            String withoutSignature = jws.substring(0, i + 1);
+            Jwt<Header, Claims> untrusted = Jwts.parser().parseClaimsJwt(withoutSignature);
+            Claims claims = untrusted.getBody();
+            String email = claims.get("email", String.class);
+            com.woorinaru.rest.dto.user.User userDto = this.userService.getByEmail(email);
+            User user = UserMapper.MAPPER.mapToModel(userDto);
             return jwtTokenUtil.generateToken(Optional.of(user));
         } else {
             return jwtTokenUtil.generateToken();
@@ -47,5 +62,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Autowired
     public void setJwtTokenUtil(JwtTokenUtil jwtTokenUtil) {
         this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 }
